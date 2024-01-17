b66232bbc95eb0098ecd4fc20ef3e43d46f3c7ed
fouadmogy10 <fouadmogy105@gmail.com>
2024-01-17 18:09:36 +0200

    first commit

---
+++ src/components/footer/Copyright.jsx
@@ -0,0 +1,13 @@
+
+function Copyright() {
+  return (
+    <div className="font-medium text-gray-300">
+      <p className=" ">
+        Privacy Policy | &copy; <span>{new Date().getFullYear()}</span> 
+      </p>
+     
+    </div>
+  );
+}
+
+export default Copyright;
